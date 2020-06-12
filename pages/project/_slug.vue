2dd416255d19a420e4f899d5d4af867b69d47294
Sekori1 <romaric55220@gmail.com>
2020-06-12 19:55:49 +0200

    -

--- pages/project/_slug.vue
+++ pages/project/_slug.vue
@@ -27,3 +27,52 @@ export default {
   }
 };
 </script>
+
+
+<style lang="scss">
+.project-page {
+    width: 100%;
+    .header {
+        max-width: 600px;
+        margin: 0 auto;
+        text-align: center;
+        .p-title {
+            margin: 15px 0;
+            font-size: 2.8em;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+    }
+
+    .project-content {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-column-gap: 1rem;
+    }
+}
+section {
+    &.head, &.description {
+        grid-column: 2/5;
+    }
+    &.illu {
+        grid-column: 1/6;
+    }
+
+    &.buttons {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        grid-column: 2/5;
+        padding-bottom: 20px;
+    }
+}
+
+@media screen and (max-width: 550px) {
+    section {
+        &.head, &.description {
+            grid-column: 1/6;
+        }
+    }
+}
+</style>
